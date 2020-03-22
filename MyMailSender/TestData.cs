9d66c1cce96e1bf2d6660d1a80c469fb71f46608
FinalCore <rimedheart@yandex.ru>
2020-03-22 21:31:18 +0300

    Добавил привязку данных к combobox с получателями

---
+++ MyMailSender/TestData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using MailSender.lib;
+
+
+namespace MyMailSender
+{
+    public static class TestData
+    {
+        public static List<Sender> Senders { get; } = new List<Sender>
+        {
+            new Sender("Пупкин", "v.pupkin@mail.ru"),
+            new Sender("Петросян", "e.petrosyan@gamil.com"),
+            new Sender("Хабибулин", "a.habibulin@yandex.ru")
+        };
+    }
+}
+
+
