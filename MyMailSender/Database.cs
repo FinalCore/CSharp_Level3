7abee91d836050c38caaa22593a1f5c0beebe820
FinalCore <rimedheart@yandex.ru>
2020-03-26 09:08:46 +0300

    Добавил в DataGrid таблицу из БД

---
+++ MyMailSender/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMailSender
+{
+    internal static class Database
+    {
+        private static readonly EmailsDataContext _emailsDataContext = new EmailsDataContext();
+
+        public static IQueryable<Recipients> Recipients => from Email in _emailsDataContext.Recipients select Email;
+    }
+}
