ff3dec34e84945f9405ab046100f16cd618a87ef
FinalCore <rimedheart@yandex.ru>
2020-04-11 16:33:03 +0300

    Сделал вычисление факториала с использованием двух потоков

---
+++ Lesson_5/Program.cs
@@ -0,0 +1,61 @@
+﻿/*   Задание
+ *   1. Написать приложение, считающее в раздельных потоках: 
+        a. факториал числа N, которое вводится с клавиатуры; 
+        b. сумму целых чисел до N.
+*/
+using System;
+using System.Threading;
+
+namespace Lesson_5
+{
+    public class Factorial
+    {
+        public int Fact { get; set; } = 1;
+        public int Number { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial arg = new Factorial();
+            Console.WriteLine("Введите число, для которого нужно вычислить факториал");
+            arg.Number = int.Parse(Console.ReadLine());
+
+            Thread thread = new Thread(()=> FactorialCalc(arg));
+
+            // запускаем вторичный поток
+            thread.Start();
+
+            // приостанавливаем первичный поток
+            Thread.Sleep(200);
+            // запускаем первичный поток
+            FactorialCalc(arg);
+            Console.WriteLine(new string('-', 25) + $"\nРезультат равен {arg.Fact}");
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// Метод, вычисляющий факториал числа
+        /// </summary>    
+        static void FactorialCalc(object arg)
+        {
+            Factorial argument = (Factorial)arg;
+                if (argument.Number == 0 || argument.Number == 1)
+                    return;
+                else
+                {
+                    while (argument.Number > 1)
+                    {
+                        argument.Fact *= argument.Number;                    
+                        Console.WriteLine($"Поток №{Thread.CurrentThread.GetHashCode()} промежуточный результат: {argument.Number}");
+                        argument.Number--;
+                        Thread.Sleep(200);
+                    }
+                   
+                }                 
+            
+        }
+    }
+}
---
+++ TestConsoleProject/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace TestConsoleProject
+{
+    class Program
+    {
+        static object lockObject = new object();
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Thread thread = new Thread(() => MyTreadMethod(ref number));
+            thread.IsBackground = true;
+
+            // запускаем воторичный поток
+            thread.Start();
+            Thread.Sleep(200);
+            // запускаем первичный поток
+            MyTreadMethod(ref number);
+            Console.WriteLine(new string('-', 15) + $"\n{number}");
+            Console.ReadKey();
+        }
+
+        #region Пример поочередного выполнения итерций цикла двумя потоками
+        static void MyTreadMethod(ref int number)
+        {
+            while (number < 10)
+            {
+                ++number;
+                Console.WriteLine($"Поток №{Thread.CurrentThread.GetHashCode()} промежуточный результат: {number}");
+                Thread.Sleep(400);
+            }
+            return;
+        }
+        #endregion
+
+        #region Пример, когда второй поток начниает работу после завершения первого
+
+        //static void MyTreadMethod(int number)
+        //{
+        //    lock(lockObject)
+        //    {
+        //        int value = 0;
+        //        for (int i = 0; i < 5; i++)
+        //        {
+        //            number++;
+        //            value++;
+        //            Console.WriteLine($"Поток №{Thread.CurrentThread.GetHashCode()} промежуточный результат: {number} тестовое число: {value}");
+        //            Thread.Sleep(200);
+        //        }
+        //        return;
+        //    }            
+        //}
+
+        #endregion
+    }
+}
